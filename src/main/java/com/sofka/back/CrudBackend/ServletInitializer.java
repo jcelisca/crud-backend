7f970df1e6da22f578d36f3abaee0d4edc275982
jcelisca <celiss1997@gmail.com>
2022-02-22 14:31:01 -0500

    Creación del proyecto

---
+++ src/main/java/com/sofka/back/CrudBackend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sofka.back.CrudBackend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CrudBackendApplication.class);
+	}
+
+}
